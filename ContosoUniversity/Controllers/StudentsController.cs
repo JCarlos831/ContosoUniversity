714ff478c913a86eda2380e5287320ab0cf4e40d
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-09-25 14:49:42 -0400

    Added seed data to database

---
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
